dcc604e3ee2c555c9a4783c6658f98f53eb101e4
erickpoleto <erick-poleto@hotmail.com>
2020-07-11 21:50:36 -0300

    RS-1 ajustes de comentarios, versoes, e nomenclaturas

--- source/app/controllers/ReportController.ts
+++ source/app/controllers/ReportController.ts
@@ -9,7 +9,6 @@ class ReportController {
       const template = TemplateService.templateManager(report)
       if (!template.error)
         return res.status(200).json({data: TemplateService.templateManager(report)})
-
       return res.status(500).send({error: "template sended with datasources incomplete"})
     } catch (e) {
       res.status(e.status).send({error: e.body});
